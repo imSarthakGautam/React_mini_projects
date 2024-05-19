1b6f0aa6915d0234f0e4446a9d18f4a3c2767076
imSarthakGautam <sarthakgtm175@gmail.com>
2024-05-19 21:05:41 +0545

    Understanding React props

--- Tailwind_and_props/src/App.jsx
+++ Tailwind_and_props/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,9 @@ function App() {
       <div className="bg-green-400">
       Tailwind test 
       </div>
+      <Card device="windows" action="read"/>
+      <Card device="Macbook" action="read"/>
+      <Card device="Linux" action="write"/>
       
     </>
   )
