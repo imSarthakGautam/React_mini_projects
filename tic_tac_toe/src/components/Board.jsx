131b4341f7474d914e8ef35a0b9d222e53b3e719
imSarthakGautam <sarthakgtm175@gmail.com>
2024-06-11 18:50:23 +0545

    useStates

--- tic_tac_toe/src/components/Board.jsx
+++ tic_tac_toe/src/components/Board.jsx
@@ -5,21 +5,21 @@ export default function Board(){
     <>
     <div className="ttt">
        <div className="boardRow"> 
-       <Square id= "1" value="1" />
-        <Square id= "2" value="2" />
-        <Square id= "3" value="3" />
+       <Square id= "1" />
+        <Square id= "2"/>
+        <Square id= "3" />
         </div>
 
         <div className="boardRow"> 
-        <Square id= "4" value="4" />
-        <Square id= "5" value="5" />
-        <Square id= "6" value="6" />
+        <Square id= "4" />
+        <Square id= "5" />
+        <Square id= "6" />
         </div>
 
         <div className="boardRow"> 
-        <Square id= "7" value="7" />
-        <Square id= "8" value="8" />
-        <Square id= "9" value="9" />
+        <Square id= "7" />
+        <Square id= "8" />
+        <Square id= "9" />
         </div>
     </div>
     </>
--- tic_tac_toe/src/components/Square.jsx
+++ tic_tac_toe/src/components/Square.jsx
@@ -1,14 +1,16 @@
 import '../App.css'
-export default function Square( {value, id}){
-   function handleClick()
-   {
-        console.log(`button ${id} clicked`)
-    }
+import { useState } from 'react';
+export default function Square( {value}){
 
+    const [value, setValue]= useState(null);
+    // value stores the value and setValue is a function that can be used to change the value.
+    // The null passed to useState is used as the initial value for this state variable, 
+    //so value here starts off equal to null.
 
+   
     return (
         <>
-        <button className='square' onClick={handleClick} id={id}> {value}</button>
+        <button className='square'  > {value}</button>
         </>
     );
         
