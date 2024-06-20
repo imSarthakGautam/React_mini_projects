e39ad199bdfa8abb9b555c4110e2b2edb7d982c8
imSarthakGautam <sarthakgtm175@gmail.com>
2024-06-20 21:30:26 +0545

    React Routers: part ii

--- React_router/src/main.jsx
+++ React_router/src/main.jsx
@@ -9,32 +9,41 @@ import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import {
-  createBrowserRouter,
-  RouterProvider,
+  Route, RouterProvider, createBrowserRouter, createRoutesFromElements
 } from "react-router-dom";
 
-const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Layout/>,
-      children: [
-        {
-          path: "",
-          element: <Home />
-        },
-        {
-          path: "about",
-          element: <About />
-        },
-        {
-          path:"contact",
-          element:<Contact />
-        }
+// const router = createBrowserRouter([
+//     {
+//       path: '/',
+//       element: <Layout/>,
+//       children: [
+//         {
+//           path: "",
+//           element: <Home />
+//         },
+//         {
+//           path: "about",
+//           element: <About />
+//         },
+//         {
+//           path:"contact",
+//           element:<Contact />
+//         }
         
-      ]
-    }
-  ])
+//       ]
+//     }
+//   ])
   
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    < Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      </Route>
+  )
+)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
